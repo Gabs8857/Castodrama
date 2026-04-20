353165f4d0b33980009700005a758b2e4e25875f
Gabs <gabmuller2111@gmail.com>
2026-04-20 11:04:10 +0200

    Refactor PlayerController and TopDownBootstrap to remove unused lighting code and optimize player setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,16 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering.Universal;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    public float playerLightIntensity = 0.7f;
-    public float playerLightOuterRadius = 3.2f;
 
     private Rigidbody2D rb;
     private Vector2 movement;
-    private Light2D playerLight;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerLight = GetComponent<Light2D>();
-        ConfigureLighting();
     }
 
     void Update()
@@ -57,30 +51,5 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocity = movement.normalized * speed;
-    }
-
-    void ConfigureLighting()
-    {
-        if (playerLight != null)
-        {
-            playerLight.enabled = true;
-            playerLight.lightType = Light2D.LightType.Point;
-            playerLight.intensity = playerLightIntensity;
-            playerLight.pointLightOuterRadius = playerLightOuterRadius;
-            playerLight.pointLightInnerRadius = 1.2f;
-            playerLight.color = new Color(1f, 0.9f, 0.63f, 1f);
-        }
-
-        GameObject globalLightObject = GameObject.Find("Global Light 2D");
-        if (globalLightObject == null)
-        {
-            return;
-        }
-
-        Light2D globalLight = globalLightObject.GetComponent<Light2D>();
-        if (globalLight != null && globalLight.intensity > 0.2f)
-        {
-            globalLight.intensity = 0.15f;
-        }
     }
 }
--- Assets/Scripts/TopDownBootstrap.cs
+++ Assets/Scripts/TopDownBootstrap.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.UI;
 
 public static class TopDownBootstrap
 {
@@ -28,8 +26,6 @@ public static class TopDownBootstrap
             playerObject.AddComponent<TopDownPlayerController>();
         }
 
-        playerObject.transform.localScale = new Vector3(0.5f, 0.75f, 1f);
-
         SpriteRenderer spriteRenderer = playerObject.GetComponent<SpriteRenderer>();
         if (spriteRenderer == null)
         {
@@ -46,40 +42,6 @@ public static class TopDownBootstrap
             hunger = playerObject.AddComponent<TopDownHunger>();
         }
 
-        GameObject globalLightObject = GameObject.Find("Global Light 2D");
-        if (globalLightObject != null)
-        {
-            Light2D globalLight = globalLightObject.GetComponent<Light2D>();
-            if (globalLight != null)
-            {
-                globalLight.enabled = false;
-            }
-        }
-
-        GameObject playerLightObject = GameObject.Find("Player Point Light");
-        if (playerLightObject == null)
-        {
-            playerLightObject = new GameObject("Player Point Light");
-        }
-
-        playerLightObject.transform.SetParent(playerObject.transform, false);
-        playerLightObject.transform.localPosition = new Vector3(0f, 0.5f, 0f);
-
-        Light2D pointLight = playerLightObject.GetComponent<Light2D>();
-        if (pointLight == null)
-        {
-            pointLight = playerLightObject.AddComponent<Light2D>();
-        }
-
-        pointLight.lightType = Light2D.LightType.Point;
-        pointLight.pointLightInnerRadius = 3.5f;
-        pointLight.pointLightOuterRadius = 10f;
-        pointLight.falloffIntensity = 0.82f;
-        pointLight.intensity = 1.35f;
-        pointLight.color = new Color(1f, 0.86f, 0.58f, 1f);
-        pointLight.lightOrder = 0;
-        pointLight.blendStyleIndex = 0;
-
         GameObject groundObject = GameObject.Find(GroundName);
         if (groundObject == null)
         {
@@ -90,7 +52,6 @@ public static class TopDownBootstrap
             groundRenderer.sprite = CreateWhiteSprite();
             groundRenderer.color = new Color(0.13f, 0.17f, 0.2f, 1f);
             groundRenderer.sortingOrder = 0;
-            groundObject.transform.localScale = new Vector3(60f, 34f, 1f);
         }
 
         Camera camera = Camera.main;
@@ -123,74 +84,6 @@ public static class TopDownBootstrap
         cameraObject.transform.position = new Vector3(playerObject.transform.position.x, playerObject.transform.position.y, -10f);
         cameraObject.transform.rotation = Quaternion.identity;
 
-        GameObject hudCanvasObject = GameObject.Find("HUD Canvas");
-        if (hudCanvasObject == null)
-        {
-            hudCanvasObject = new GameObject("HUD Canvas");
-            Canvas canvas = hudCanvasObject.AddComponent<Canvas>();
-            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-            canvas.sortingOrder = 100;
-
-            CanvasScaler canvasScaler = hudCanvasObject.AddComponent<CanvasScaler>();
-            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-            canvasScaler.referenceResolution = new Vector2(1920f, 1080f);
-            canvasScaler.matchWidthOrHeight = 0.5f;
-
-            hudCanvasObject.AddComponent<GraphicRaycaster>();
-        }
-
-        GameObject hungerFrame = GameObject.Find("Hunger Frame");
-        if (hungerFrame == null)
-        {
-            hungerFrame = new GameObject("Hunger Frame");
-            hungerFrame.transform.SetParent(hudCanvasObject.transform, false);
-
-            RectTransform frameRect = hungerFrame.AddComponent<RectTransform>();
-            frameRect.anchorMin = new Vector2(0f, 0f);
-            frameRect.anchorMax = new Vector2(0f, 0f);
-            frameRect.pivot = new Vector2(0f, 0f);
-            frameRect.anchoredPosition = new Vector2(24f, 24f);
-            frameRect.sizeDelta = new Vector2(240f, 20f);
-
-            Image backgroundImage = hungerFrame.AddComponent<Image>();
-            backgroundImage.sprite = CreateWhiteSprite();
-            backgroundImage.color = new Color(0f, 0f, 0f, 0.55f);
-
-            GameObject hungerFill = new GameObject("Hunger Fill");
-            hungerFill.transform.SetParent(hungerFrame.transform, false);
-
-            RectTransform fillRect = hungerFill.AddComponent<RectTransform>();
-            fillRect.anchorMin = new Vector2(0f, 0f);
-            fillRect.anchorMax = new Vector2(1f, 1f);
-            fillRect.offsetMin = new Vector2(3f, 3f);
-            fillRect.offsetMax = new Vector2(-3f, -3f);
-
-            Image fillImage = hungerFill.AddComponent<Image>();
-            fillImage.sprite = CreateWhiteSprite();
-            fillImage.type = Image.Type.Filled;
-            fillImage.fillMethod = Image.FillMethod.Horizontal;
-            fillImage.fillOrigin = (int)Image.OriginHorizontal.Left;
-            fillImage.fillAmount = 1f;
-
-            TopDownHungerBarUI hungerBarUI = hungerFrame.AddComponent<TopDownHungerBarUI>();
-            hungerBarUI.Hunger = hunger;
-            hungerBarUI.FillImage = fillImage;
-        }
-        else
-        {
-            TopDownHungerBarUI hungerBarUI = hungerFrame.GetComponent<TopDownHungerBarUI>();
-            if (hungerBarUI == null)
-            {
-                hungerBarUI = hungerFrame.AddComponent<TopDownHungerBarUI>();
-            }
-
-            hungerBarUI.Hunger = hunger;
-            Image fillImage = hungerFrame.transform.Find("Hunger Fill")?.GetComponent<Image>();
-            if (fillImage != null)
-            {
-                hungerBarUI.FillImage = fillImage;
-            }
-        }
     }
 
     private static Sprite CreateWhiteSprite()
